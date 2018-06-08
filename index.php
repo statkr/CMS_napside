a41da89d241d7a78bdda23612b0faf4c296253ef
admin <admin@ONEPLUS-A5000.Dlink>
2018-06-08 17:36:08 +0400

    fix

--- index.php
+++ index.php
@@ -1,43 +1,6 @@
 <?php
 
-/**
- * Flexo CMS - Content Management System. <http://flexo.up.dn.ua>
- * Copyright (C) 2008 Maslakov Alexander <jmas.ukraine@gmail.com>
- * Copyright (C) 2008 Philippe Archambault <philippe.archambault@gmail.com>
- * Copyright (C) 2008 Martijn van der Kleijn <martijn.niji@gmail.com>
- *
- * This file is part of Frog CMS.
- *
- * Frog CMS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Frog CMS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Frog CMS.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Frog CMS has made an exception to the GNU General Public License for plugins.
- * See exception.txt for details and the full text.
- */
-
-/**
- * The Framework file is a modified version of the so-called Green Framework.
- * 
- * @package framework
- *
- * @author Maslakov Alexander <jmas.ukraine@gmail.com>
- * @version 0.1.0
- * @license http://www.gnu.org/licenses/gpl.html GPL License
- * @copyright Maslakov Alexander, 2011
- */
-
-// CMS version
-define('CMS_VERSION', '0.1.0 rc1');
+define('CMS_VERSION', '0.1.6 design alfa');
 
 // Base pathes
 define('CMS_ROOT',          dirname(__FILE__));
